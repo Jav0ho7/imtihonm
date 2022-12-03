f02fcb79c5ef2a5adaee83c81b6ebc5d32542f57
Al18ek <sattarovalibek8@gmail.com>
2022-12-03 16:37:54 +0500

    first commit

---
+++ js/minu.js
@@ -0,0 +1,112 @@
+
+
+
+//SAVOLLAR//
+
+/*J 1 : o'zgaruvchanlar
+
+1var  ; eski deyarli ishlatilmaydi
+2let  ; aktiv ozgaruvchan
+3const ; ozgarmas ozgaruvchan
+
+J 2 :
+
+1.string
+2.number
+3.BigInt
+4.boolean
+5.object
+6.underfined
+7.null
+8.Symbol
+
+J 3 :
+
+increment ++ qoshadi +1+
+decrement -- ayiradi -1-
+
+J 4 :
+
+string + number =
+
+J 5 :
+
+&& = "i"
+|| = "va"
+
+
+J 6 :
+
+if   = true
+else = folse
+
+J 7 :
+
+if(??){
+
+}else{
+  ?????
+
+
+ if else (???){
+  }
+}
+
+J 8 :MASSIV
+
+massiv malumotlarni index boyicha saqlaydi . agar ohirgi elementini olmoqchi bolsang lenght - 1 yoziladi
+
+J 9 : OBJECT
+
+object (key bn veliusi) yordamida ishlaydi
+
+J 10 : METHOD
+
+method = oz ichiga ( .push .shift .pop .unshift .lenght .map .includes  .reverse .reduse  )
+
+*/
+
+//MASALALAR//
+
+// 1-masala //
+
+ let myFunc = (a, b) =>{
+   if(typeof(a) ==="number" || typeof b === "number"){
+  
+     return (a + b)
+   }
+   else{
+      console.log("son emas")
+  }
+
+ }
+ console.log(myFunc(93,09)) 
+
+//2 - masala
+
+let myFunc2 = (a, b) =>{
+  if(typeof(a) ==="number" || typeof b === "number"){
+ 
+    return (a - b)
+  }
+  else{
+     console.log("son emas")
+ }
+
+}
+console.log(myFunc2(2022,15, )) 
+
+
+//3-masala//
+
+let myFunc3 = (a, b) =>{
+  if(typeof(a) ==="number" || typeof b === "number"){
+ 
+    return (a + b)
+  }
+  else{
+     console.log("son emas")
+ }
+
+}
+console.log(myFunc3()) 
